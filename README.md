9891875c5b8f2274c7403e0d54a70fed55ee670b
ATOMICMAN007 <faizankhanf8875@gmail.com>
2023-07-17 18:30:36 +0530

    Made article summary render as markup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RSS Feed App
 Simple RSS feed reader for my convenience and learning about RSS feeds in general.
+
+## TODO
+- [ ] Refresh feed functionaity
+- [ ] Multiple RSS feed subscriptions
+- [ ] Add Sidebar with RSS subscribtions 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import feedparser
+from markupsafe import Markup 
 
 data = feedparser.parse("./dummy.xml")
 entries = data.entries
@@ -7,6 +8,12 @@ entries = data.entries
 app = Flask(__name__)
 
 
+def markup_entries(entries):
+    for entry in entries:
+        entry.summary = Markup(entry.summary)
+    return entries
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", lentries = len(entries), entries=entries)
+    return render_template("index.html", entries=markup_entries(entries=entries))
